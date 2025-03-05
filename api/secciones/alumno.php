efb08aabdd3ed8de4da545d9e130a63e690e2abe
Cruz20025 <202014024@uns.edu.pe>
2025-03-05 11:27:07 -0500

    Update and rename prueba.txt to alumno.php

---
+++ api/secciones/alumno.php
@@ -0,0 +1,11 @@
+<?php
+
+$alumno = [
+"nombres" => "Ruben Gonzalo",
+"apellidos" => "Cruz Acosta",
+"edad" => "24 años",
+"telefono" => "996701532",
+"correo" => "acostakaku420@gmail.com",
+
+];
+?>
